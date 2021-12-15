568e29ae0a3469c1ae1508f5b8117dd8d4a95aaa
Muhammad Arya Danuarta <72311684+danuartax64@users.noreply.github.com>
2021-12-15 20:22:23 +0700

    Update sortkode.py

--- sortkode.py
+++ sortkode.py
@@ -1,7 +1,6 @@
 import csv
 file = csv.reader(open('tesuas.csv', 'r'))
-rows = [column for column in file]
-
+rows = [row for row in file]
 
 # Python program for implementation of Bubble Sort
 # from: https://www.geeksforgeeks.org/python-program-for-bubble-sort/
@@ -12,7 +11,7 @@ def bubble_sort(arr):
     for i in range(n - 1):
         # range(n) also work but outer loop will repeat one time more than needed.
         # Last i elements are already in place
-        for j in range(0, n - i - 1):
+        for j in range(1, n - i - 1):
             # traverse the array from 0 to n-i-1
             # Swap if the element found is greater
             # than the next element
@@ -20,8 +19,17 @@ def bubble_sort(arr):
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
     return arr
+print("Data sebelum diurutkan: ")
 
+for i in range(5):
+    print(rows[i])
 
-sorted_rows = [', '.join(row)+'\n' for row in bubble_sort(rows)]
-with open('outputtesuas.csv', 'w') as f:
-    f.writelines(sorted_rows)
+sorted_rows = bubble_sort(rows)
+
+print("Data setelah diurutkan: ")
+for i in range(5):
+    print(sorted_rows[i])
+    
+#with open('outputtesuas.csv', 'w') as f:
+       #sorted_rows2 = [', '.join(row)+'\n' for row in bubble_sort(rows)]
+       #f.writelines(sorted_rows2)
