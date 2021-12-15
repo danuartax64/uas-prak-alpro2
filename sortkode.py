00e19b640577dd07e16bb839006ae6a74d04facb
Muhammad Arya Danuarta <72311684+danuartax64@users.noreply.github.com>
2021-12-15 23:14:52 +0700

    Update sortkode.py

--- sortkode.py
+++ sortkode.py
@@ -2,6 +2,8 @@ import csv
 import os
 from timeit import default_timer as timer
 
+os.chdir(r"C:\Users\User\Documents\python2")
+
 file = csv.reader(open('tesuas.csv', 'r'))
 rows = [row for row in file]
 
@@ -26,42 +28,54 @@ def binary_search(arr, low, high, x):
         return -1
         
 def clear():
-    os.system('clear')		#kalo di linux clear kalo di windows cls
+    os.system('cls')		#kalo di linux clear kalo di windows cls
     
-print("Data sebelum diurutkan: ")
+print("Sampel data sebelum diurutkan: ")
 for i in range(5):
     print(rows[i])
 
+start = timer()
 sorted_rows = bubble_sort(rows)
+end = timer()
+elapsedtime = end - start
+lewat = "{:.2f}".format(elapsedtime)
 
-print("Data setelah diurutkan: ")
+print("\nSampel data setelah diurutkan: ")
 for i in range(5):
     print(sorted_rows[i])
-
+print(f"\nWaktu sort: {lewat} detik")
 while True:
-    menu = int(input("\n1. Cari nilai tertentu dalam variabel sales\n2. Tulis hasil sort ke file baru\n3.Exit\n"))
+    print("==========================================================================")
+    menu = int(input("MENU\n1. Cari nilai tertentu dalam variabel sales\n2. Tulis hasil sort ke file baru\n3.Exit\n"))
     if menu == 1:
         cari = input("Nilai berapa yang ingin dicari: ")
         hasil = binary_search(sorted_rows, 1, len(sorted_rows)-1, cari)
+
         if hasil != -1:
             clear()	
-            print(f"Nilai {cari} ada pada variabel sales pada baris ke-{hasil}")
+            print(f"Nilai {cari} ada pada variabel sales pada baris ke-{hasil}\n")
             print(sorted_rows[0])
             print(sorted_rows[hasil])
+            input("\nTekan ENTER untuk melanjutkan...")
+            clear()
+        else:
+            print(f"\nNilai {cari} tidak ditemukan pada variabel sales")
             input("Tekan ENTER untuk melanjutkan...")
             clear()
+
     elif menu == 2:
         start = timer()
-        with open('outputtesuas.csv', 'w') as f:
+        with open('outputtesuas.csv', 'w+') as f:
             sorted_rows2 = [', '.join(row)+'\n' for row in bubble_sort(rows)]
             f.writelines(sorted_rows2)
             end = timer()
-            print("File hasil sort telah dibuat dengan nama outputtesuas.csv!")
             elapsedtime = end - start
             lewat = "{:.2f}".format(elapsedtime)
+            print("File hasil sort telah dibuat dengan nama outputtesuas.csv!")
             print(f"Dengan waktu proses selama {lewat} detik")
             input("Tekan ENTER untuk melanjutkan")
             clear()
+
     elif menu == 3:
         print("Keluar...")
         break
